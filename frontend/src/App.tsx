aed98d1eecd61a05ae672e39cb37497d474e6314
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2026-03-31 14:47:48 +0100

    fix: fix constant get request error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useLeagueStore from "../src/services/leagueService"
 
-type League = {
-    id: number,
-    name: string
-}
-
-interface LeagueState {
-    leagues: League[],
-    getLeagues: () => void;
-}
 
 function App() {
   const { leagues, getLeagues } = useLeagueStore();
