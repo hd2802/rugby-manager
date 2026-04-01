2dbcd75906fd5075c6d277f273d665ac04f59b02
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2026-04-01 15:16:08 +0100

    Revert "changes stuff"
    This reverts commit 7178960086b8cba9b66de4613656369f89d456f6.

--- frontend/src/components/app-header.tsx
+++ frontend/src/components/app-header.tsx
@@ -2,27 +2,21 @@ import {
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
+  NavigationMenuLink,
 } from "@/components/ui/navigation-menu"
-
-import {
-    Button
-} from "@/components/ui/button"
 
 export function AppHeader() {
     return (
-          <header className="w-full bg-background px-4 py-2 flex items-center relative z-20 md:pl-[5rem] ml-8 mt-2 flex">
-            <NavigationMenu>
+      <header className="w-full bg-background px-4 py-2 flex items-center relative z-20 md:pl-[5rem]">
+          <NavigationMenu>
             <NavigationMenuList>
-              <NavigationMenuItem className="flex-1">
-                <span className="font-bold text-2xl">Team Name</span>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/">Home</NavigationMenuLink>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <div className="font-bold ml-4 border-l border-text-primary p-4">1st April 2026</div>
+                <NavigationMenuLink href="/about">About</NavigationMenuLink>
               </NavigationMenuItem>
-
-              <NavigationMenuItem>
-                <Button>Continue</Button>
-              </NavigationMenuItem>
+              
             </NavigationMenuList>
           </NavigationMenu>
         </header>
--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -10,9 +10,9 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <SidebarProvider>
-      <div className="flex flex-col h-screen w-screen bg-base text-text-primary font-manrope">
+      <div className="flex flex-col h-screen w-screen bg-base text-text-primary">
         <AppHeader />
-        <div className="flex flex-1 min-h-0 m-8">
+        <div className="flex flex-1 min-h-0">
           <AppSidebar />
           <main className="flex-1 p-4 overflow-auto">
             {children}
