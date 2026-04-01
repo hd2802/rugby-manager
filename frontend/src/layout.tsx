f96807472f5b5d914d84b7c0d1165885e3ba1d06
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2026-04-01 15:16:16 +0100

    Revert "feat: adds basic sidebar"
    This reverts commit 7892b12cdd1588c3679086b14a751aba62a31931.

--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -1,6 +1,4 @@
 import type { ReactNode } from "react";
-import { SidebarProvider } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar.tsx"
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,13 +6,10 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="flex h-screen w-screen bg-base text-text-primary">
-        <main className="flex-1 p-4 overflow-auto">
-          {children}
-        </main>
-      </div>
-    </SidebarProvider>
+    <div className="flex h-screen bg-base text-text-primary">
+      <main className="flex-1 p-4 overflow-auto">
+        {children}
+      </main>
+    </div>
   );
 }
